3589adbef77778db726e6be69f3c8df9b9fc3c80
Xoaine <xoaine@hotmail.com>
2025-09-03 09:12:17 +0700

    Update Locketgold.js

--- js/Locketgold.js
+++ js/Locketgold.js
@@ -1,8 +1,37 @@
-// ========= ID ========= //
-const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-  'Locket': ['Gold']
+// Locket Premium Unlock (15s video) - port for Stash
+// Author: Ohoang7 | Fix by xoaine
+
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var obj = JSON.parse($response.body);
+
+obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
+
+var ohoang7 = {
+  is_sandbox: false,
+  ownership_type: "PURCHASED",
+  billing_issues_detected_at: null,
+  period_type: "normal",
+  expires_date: "2099-12-18T01:04:17Z",
+  grace_period_expires_date: null,
+  unsubscribe_detected_at: null,
+  original_purchase_date: "2024-07-28T01:04:18Z",
+  purchase_date: "2024-07-28T01:04:17Z",
+  store: "app_store"
 };
-// =========   Phần cố định  ========= // 
-// =========  @Ohoang7 ========= // 
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";var ohoang7={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2024-07-28T01:04:18Z",purchase_date:"2024-07-28T01:04:17Z",store:"app_store"},vuong2023={grace_period_expires_date:null,purchase_date:"2024-07-28T01:04:17Z",product_identifier:"com.ohoang7.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(vuong2023.product_identifier=s,obj.subscriber.subscriptions[s]=ohoang7):obj.subscriber.subscriptions["com.ohoang7.premium.yearly"]=ohoang7,obj.subscriber.entitlements[e]=vuong2023}else obj.subscriber.subscriptions["com.ohoang7.premium.yearly"]=ohoang7,obj.subscriber.entitlements.pro=vuong2023;$done({body:JSON.stringify(obj)});
+
+var vuong2023 = {
+  grace_period_expires_date: null,
+  purchase_date: "2024-07-28T01:04:17Z",
+  product_identifier: "com.ohoang7.premium.yearly",
+  expires_date: "2099-12-18T01:04:17Z"
+};
+
+// Luôn ép entitlement Gold
+obj.subscriber.subscriptions["com.ohoang7.premium.yearly"] = ohoang7;
+obj.subscriber.entitlements["Gold"] = vuong2023;
+
+// Thêm entitlement vip+watch_vip để mở 15s video
+obj.subscriber.subscriptions["vip+watch_vip"] = ohoang7;
+obj.subscriber.entitlements["vip+watch_vip"] = vuong2023;
+
+$done({ body: JSON.stringify(obj) });
